f93961589ff616ba89f182ea4edb929eabb6f239
Qurat-ul-Ain00 <quratulain2805@gmail.com>
2024-11-13 11:58:08 +0500

    Add files via upload

---
+++ kg to pounds.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double KG, P;
+	cout << "Enter the weight in kilogram= ";
+	cin >> KG;
+	
+	P = KG * 2.2;
+	cout << "Value of weight in pounds= " << P;
+	return 0;
+}
